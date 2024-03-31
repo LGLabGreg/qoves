e474cb97d2e4ecfc8e3d6888cfafbe3121d7d442
LGLabGreg <greg@lglab.co.uk>
2024-03-31 17:13:04 +0100

    image content overlay

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,42 +1,67 @@
-import { useAnimate, motion } from "framer-motion";
+import { motion } from "framer-motion";
 
 import homeImage from "@/assets/home.jpg";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AnimatedText } from "@/components/animated-text";
 
 import { serif } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
 import { cubicBezier } from "@/lib/animations";
 
+interface ContainerDimensionsProps {
+  width: number;
+  height: number;
+}
+
 const Home = () => {
-  const [scope, animate] = useAnimate();
+  const [imageContainerDimensions, setImageContainerDimensions] =
+    useState<ContainerDimensionsProps>({ width: 0, height: 0 });
+  const [contentBoxContainerDimensions, setContentBoxContainerDimensions] =
+    useState<ContainerDimensionsProps>({ width: 0, height: 0 });
+
+  const imageContainerRef = useRef<HTMLDivElement | null>(null);
+  const contentBoxContainerRef = useRef<HTMLDivElement | null>(null);
+
   useEffect(() => {
-    animate(
-      ".image-container",
-      { transform: "translateX(0)" },
-      {
-        duration: 1,
-        ease: cubicBezier,
-      }
-    );
-    animate(
-      ".image-reveal",
-      { width: 0 },
-      {
-        duration: 1,
-        ease: cubicBezier,
-      }
-    );
-    animate(
-      ".content-box",
-      { width: 312, height: 400 },
-      {
-        duration: 1,
-        delay: 2,
-        ease: cubicBezier,
-      }
-    );
-  }, []);
+    if (imageContainerRef.current) {
+      setImageContainerDimensions({
+        width: imageContainerRef.current.clientWidth,
+        height: imageContainerRef.current.clientHeight,
+      });
+    }
+    if (contentBoxContainerRef.current) {
+      setContentBoxContainerDimensions({
+        width: contentBoxContainerRef.current.clientWidth,
+        height: contentBoxContainerRef.current.clientHeight,
+      });
+    }
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, [imageContainerRef]);
+
+  const handleResize = () => {
+    if (imageContainerRef.current) {
+      setImageContainerDimensions({
+        width: imageContainerRef.current.clientWidth,
+        height: imageContainerRef.current.clientHeight,
+      });
+    }
+    if (contentBoxContainerRef.current) {
+      setContentBoxContainerDimensions({
+        width: contentBoxContainerRef.current.clientWidth,
+        height: contentBoxContainerRef.current.clientHeight,
+      });
+    }
+  };
+
+  function onContentBoxUpdate(latest: { width: number; height: number }) {
+    setContentBoxContainerDimensions({
+      width: Math.round(latest.width),
+      height: Math.round(latest.height),
+    });
+  }
 
   return (
     <motion.div
@@ -46,7 +71,6 @@ const Home = () => {
       exit={{ opacity: 0 }}
       transition={{ duration: 0.2 }}
       className="lg:flex h-screen"
-      ref={scope}
     >
       <div className="lg:w-1/2 flex flex-col">
         <div className="flex h-[calc(100%-274px)]">
@@ -134,14 +158,118 @@ const Home = () => {
           <div className="w-[40%] border-l p-6 border-t"></div>
         </div>
       </div>
-      <div className="relative lg:w-1/2 transform translate-x-[100%] image-container">
-        <div className="absolute z-10 bg-background top-0 left-0 w-[40%] h-full image-reveal"></div>
+      <motion.div
+        animate={{
+          transform: "translateX(0)",
+        }}
+        transition={{ duration: 1, ease: cubicBezier }}
+        className="relative lg:w-1/2 transform translate-x-[100%] image-container"
+        ref={imageContainerRef}
+      >
+        <motion.div
+          animate={{
+            width: 0,
+          }}
+          transition={{ duration: 1, ease: cubicBezier }}
+          className="absolute z-10 bg-background top-0 left-0 w-[40%] h-full image-reveal"
+        ></motion.div>
         <div
-          className="w-full h-screen bg-cover bg-center aspect-square"
+          className="w-full h-screen bg-cover bg-center"
           style={{ backgroundImage: `url(${homeImage.src})` }}
         ></div>
-        <div className="absolute top-1/2 left-1/2 w-full h-full -translate-x-1/2 -translate-y-1/2 backdrop-filter backdrop-brightness-75 content-box"></div>
-      </div>
+        <motion.div
+          animate={{
+            width: 312,
+            height: 400,
+            opacity: 1,
+          }}
+          transition={{ duration: 0.6, delay: 2, ease: cubicBezier }}
+          className="absolute overflow-hidden top-1/2 left-1/2 w-full h-full -translate-x-1/2 -translate-y-1/2 spin-container p-[1px] z-[1] opacity-0"
+        >
+          <div className="spinning-border z-[2]"></div>
+          <div className="relative w-full h-full bg-black z-[3]"></div>
+        </motion.div>
+
+        <motion.div
+          animate={{
+            width: 310,
+            height: 398,
+            opacity: 1,
+          }}
+          transition={{ duration: 0.8, delay: 2, ease: cubicBezier }}
+          onUpdate={onContentBoxUpdate}
+          className="absolute overflow-hidden top-1/2 left-1/2 w-full h-full -translate-x-1/2 -translate-y-1/2 z-[10] content-box opacity-0"
+          ref={contentBoxContainerRef}
+        >
+          <div className="w-[20px] h-[20px] top-0 left-0 absolute border-t-[2px] border-l-[2px] border-white z-10"></div>
+          <div className="w-[20px] h-[20px] right-0 bottom-0 absolute border-b-[2px] border-r-[2px] border-white z-10"></div>
+          <div
+            className="bg-cover bg-center absolute"
+            style={{
+              backgroundImage: `url(${homeImage.src})`,
+              width: `${imageContainerDimensions.width}px`,
+              height: `${imageContainerDimensions.height}px`,
+              left: `-${
+                (imageContainerDimensions.width -
+                  contentBoxContainerDimensions.width) /
+                2
+              }px`,
+              top: `-${
+                (imageContainerDimensions.height -
+                  contentBoxContainerDimensions.height) /
+                2
+              }px`,
+            }}
+          ></div>
+          <div className="absolute w-full h-full inset-0 backdrop-filter backdrop-brightness-[65%] p-5 pb-2 flex flex-col text-white">
+            <AnimatedText
+              el="p"
+              text={["Nose", "assessment", "report"]}
+              className="text-3xl leading-7 "
+              delay={1500}
+              animateLines={true}
+              animation={{
+                hidden: {
+                  opacity: 0,
+                  x: -500,
+                },
+                visible: {
+                  opacity: 1,
+                  x: 0,
+                  transition: {
+                    duration: 1,
+                    ease: cubicBezier,
+                  },
+                },
+              }}
+            />
+            <div className="relative mt-auto">
+              <motion.div
+                animate={{
+                  width: 270,
+                  left: 0,
+                }}
+                transition={{ duration: 0.6, delay: 2.3, ease: cubicBezier }}
+                className="absolute w-0 h-[1px] bg-white/50 top-0 left-[25%]"
+              ></motion.div>
+              <div className="flex pt-2 uppercase">
+                <div className="w-1/3">
+                  <div className="text-white/60 leading-3">Period/</div>
+                  <div className=" text-lg">2 weeks</div>
+                </div>
+                <div className="w-1/3">
+                  <div className="text-white/60 leading-3">Parts/</div>
+                  <div className=" text-lg">8</div>
+                </div>
+                <div className="w-1/3">
+                  <div className="text-white/60 leading-3">Pages/</div>
+                  <div className=" text-lg">112</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </motion.div>
+      </motion.div>
     </motion.div>
   );
 };
